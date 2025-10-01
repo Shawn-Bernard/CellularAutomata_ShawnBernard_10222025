4da9e92bc57f6138bbe1103fb7605c1087bb3df3
Shawn Bernard <ssbernard@outlook.com>
2025-10-01 10:27:37 -0300

    Added the start of map generator

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,16 +1,73 @@
+using System;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public int width;
+    public int height;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0,100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GenerateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+        // pseudo random number generator
+        System.Random prng = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[x, y] = (prng.Next(0, 100) < randomFillPercent)? 1 : 0;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 position = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    Gizmos.DrawCube(position, Vector3.one);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Map is null");
+        }
     }
 }
