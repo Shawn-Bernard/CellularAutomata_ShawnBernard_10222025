62fa9a972e9584f81cb3e82823a366555bfa03c3
Shawn Bernard <ssbernard@outlook.com>
2025-10-08 10:27:01 -0300

    Added neighbour count

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -21,16 +21,12 @@ public class MapGenerator : MonoBehaviour
         GenerateMap();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void GenerateMap()
     {
         map = new int[width, height];
         RandomFillMap();
+
+        SmoothMap();
     }
 
     void RandomFillMap()
@@ -46,28 +42,83 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                map[x, y] = (prng.Next(0, 100) < randomFillPercent)? 1 : 0;
+                if (x == 0 || x == width-1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (prng.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+                
             }
         }
     }
 
-    private void OnDrawGizmos()
+    void SmoothMap()
     {
-        if (map != null)
+        for (int x = 0; x < width; x++)
         {
-            for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
             {
-                for (int y = 0; y < height; y++)
+                int neighrbourWallTiles = GetSurroundingWallCount(x, y);
+                
+                if (neighrbourWallTiles > 4)
                 {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 position = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(position, Vector3.one);
+                    map[x, y] = 1;
+                }
+                else if (neighrbourWallTiles < 4)
+                {
+                    map[x, y] = 0;
                 }
             }
         }
-        else
+    }
+
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX-1; neighbourX <= gridX; neighbourX++)
         {
-            Debug.Log("Map is null");
+            for (int neighbourY = gridY - 1; neighbourY <= gridY; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
         }
+        return wallCount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            if (map != null)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                        Vector3 position = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                        Gizmos.DrawCube(position, Vector3.one);
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Map is null");
+            }
+        }
+        
     }
 }
